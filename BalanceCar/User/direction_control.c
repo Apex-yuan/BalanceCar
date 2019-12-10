0dbc13288af382da0f3ecfd13c1dc5f1a460ac55
Apex-yuan <wangyuan090693@163.com>
2019-12-10 10:17:32 +0800

    规范direction_control驱动代码，并修改相关联位置代码

--- BalanceCar/User/direction_control.c
+++ BalanceCar/User/direction_control.c
@@ -1,13 +1,37 @@
+/**
+  ******************************************************************************
+  * @file    direction_control.c 
+  * @author  Apexyuan
+  * @version V1.0.0
+  * @date    2019-12-10
+  * @brief   方向控制相关代码
+  ******************************************************************************
+  * @attention
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/ 
 #include "direction_control.h"
 
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
 float g_fDirectionControlOut = 0;
 float g_fDirectionControlNew, g_fDirectionControlOld;
 uint8_t g_nDirectionControlPeriod;
 uint16_t g_nDirectionControlCount;
-
 float g_fDirectionSet;
+/* Private function prototypes -----------------------------------------------*/
 
-void DirectionControl()
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+void DirectionControl(void)
 {
 //  float fValue; 
   g_fDirectionControlNew = g_fDirectionSet;
@@ -15,7 +39,11 @@ void DirectionControl()
   g_fDirectionControlOld = g_fDirectionControlNew;
 }
 
-
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
 void DirectionControlOutput(void)
 {
   float fValue;
--- BalanceCar/User/direction_control.h
+++ BalanceCar/User/direction_control.h
@@ -1,23 +1,47 @@
+/**
+  ******************************************************************************
+  * @file    direction_control.h 
+  * @author  Apexyuan
+  * @version V1.0.0
+  * @date    2019-12-10
+  * @brief   方向控制相关代码
+  ******************************************************************************
+  * @attention
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __DIRECTION_CONTROL_H
 #define __DIRECTION_CONTROL_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+/* Includes ------------------------------------------------------------------*/
 #include "stdint.h"
 
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
 #define DIRECTION_CONTROL_PERIOD 10
 
-
-
+/* Exported variables ------------------------------------------------------- */
 extern float g_fDirectionControlOut;
 extern float g_fDirectionControlNew, g_fDirectionControlOld;
 extern uint8_t g_nDirectionControlPeriod;
 extern uint16_t g_nDirectionControlCount;
 
 extern float g_fDirectionSet;
-
+/* Exported functions ------------------------------------------------------- */
 void DirectionControl(void);
 void DirectionControlOutput(void);
 
-#endif /* __DIRECTION_CONTROL_H */
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __DIRECTION_CONTROL_H */
 
 
